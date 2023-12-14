5daeffbddb15958398efa3166260e35dd2ec8292
Mr_Thibault <133662354+nineInchClous@users.noreply.github.com>
2023-12-14 08:18:48 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// #region Imports
+import './css/styles.scss';
+
+// #endregion
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+});
---
+++ webpack.common.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader', 
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
